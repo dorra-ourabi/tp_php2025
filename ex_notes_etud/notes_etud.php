5e07973185a0c077445906cf798c4d9bf92b6ae6
dorra dorra <dorra.ourabi@insat.ucar.tn>
2025-04-02 19:58:45 +0100

    fixing a bug

--- ex_notes_etud/notes_etud.php
+++ ex_notes_etud/notes_etud.php
@@ -15,91 +15,91 @@
 </head>
 
 <?php
-class Etudiant{
+class Etudiant {
     private $nom;
-    private $notes=array();
-    public function __construct($name,$notes){
-        $nom=$name;
-        $this->notes=$notes;
+    private $notes = array();
+
+    // Constructor to initialize name and notes
+    public function __construct($name, $notes) {
+        $this->nom = $name;  // Corrected property assignment
+        $this->notes = $notes;
     }
-    private function afficher_note(){
-        foreach($this->$notes as $value){
-            echo 'note:'.$value ;
+
+    // Display all notes
+    public function afficher_notes() {
+        foreach ($this->notes as $value) {  // Corrected $notes reference
+            echo 'Note: ' . $value . '<br>';
         }
-    
     }
-    private function calculer_moyenne(){
-        $nb_notes=func_num_args();
-        $somme=0;
-        foreach($this->$notes as $value){
-          $somme+=$value;
-    }
-    return ($somme/$nb_notes); 
 
+    // Calculate average
+    public function calculer_moyenne() {
+        $nb_notes = count($this->notes); // Get number of notes
+        if ($nb_notes == 0) return 0; // Avoid division by zero
+
+        $somme = array_sum($this->notes); // Sum all notes
+        return $somme / $nb_notes;
+    }
+
+    // Determine if the student is successful
+    public function success() {
+        $moy = $this->calculer_moyenne();
+        if ($moy < 10) {
+            echo $this->nom . ' : Non admis <br>';
+        } else {
+            echo $this->nom . ' : ADMIS! <br>';
+        }
+    }
 }
-function success(){
-    $moy=$this->calculer_moyenne();
-    if($moy<10){
-        echo'non admis';
-    }
-    else{
-        echo'ADMIS!';
-    }
-}};
-?>
 
-<?php
-    $etudiants = [
-        new Etudiant("Aymen", [11, 13, 18, 7, 10, 13, 2, 5, 1]),
-        new Etudiant("Skander", [15, 9, 8, 16])
-    ];
-    $moya=$etudiants[0].calculer_moyenne();
-    $moys=$s[1].calculer_moyenne();
-    
-?>
+// Creating student objects
+$aymen = new Etudiant("Aymen", [11, 13, 18, 7, 10, 13, 2, 5, 1]);
+$skander = new Etudiant("Skander", [15, 9, 8, 16]);
+$moya=$aymen->calculer_moyenne();
+$moys=$skander->calculer_moyenne();?>
 
 <body>
     <div class="row">
     <div class="col container" id="aymen">
     <form action="notes_etud.php" method="post" >
     <div class="form-floating mb-3">
-       <input type="text" name="nom" class="form-control" id="floatingInput">
+       <input type="text" name="nom" value="<?php echo 'Aymen'?>" class="form-control" id="floatingInput">
   
     </div>
     <div class="form-floating mb-3">
-      <input type="text" name="note1" class="form-control" id="floatingPassword" >
+      <input type="text" name="note1" value="<?php echo '11'?>" class="form-control" id="floatingPassword" >
   
     </div>
     <div class="form-floating mb-3">
-       <input type="text" name="note2" class="form-control" id="floatingInput">
+       <input type="text" name="note2" value="<?php echo '13'?>" class="form-control" id="floatingInput">
   
     </div>
     <div class="form-floating mb-3">
-      <input type="text" name="note 3" class="form-control" id="floatingPassword" >
+      <input type="text" name="note 3"  value="<?php echo '18'?>"class="form-control" id="floatingPassword" >
   
     </div>
     <div class="form-floating mb-3">
-       <input type="text" name="note 4" class="form-control" id="floatingInput">
+       <input type="text" name="note 4"  value="<?php echo '7'?>" class="form-control" id="floatingInput">
   
     </div>
     <div class="form-floating mb-3">
-      <input type="text" name="note 5" class="form-control" id="floatingPassword" >
+      <input type="text" name="note 5"  value="<?php echo '10'?>" class="form-control" id="floatingPassword" >
   
     </div>
     <div class="form-floating mb-3">
-       <input type="text" name ="note 6"class="form-control" id="floatingInput">
+       <input type="text" name ="note 6"  value="<?php echo '13'?>"class="form-control" id="floatingInput">
   
     </div>
     <div class="form-floating mb-3">
-      <input type="text" name="note 7" class="form-control" id="floatingPassword" >
+      <input type="text" name="note 7"  value="<?php echo '2'?>" class="form-control" id="floatingPassword" >
   
     </div>
     <div class="form-floating mb-3">
-      <input type="text" name="note 8" class="form-control" id="floatingPassword" >
+      <input type="text" name="note 8"  value="<?php echo '5'?>" class="form-control" id="floatingPassword" >
   
     </div>
     <div class="form-floating mb-3">
-      <input type="text" name="note 9" class="form-control" id="floatingPassword" >
+      <input type="text" name="note 9"   value="<?php echo '1'?>"class="form-control" id="floatingPassword" >
   
     </div>
     <div class="form-floating mb-3">
@@ -117,27 +117,27 @@ function success(){
     
 <div class="col container" id= "skander">
     <div class="form-floating mb-3">
-       <input type="text" class="form-control" id="floatingInput">
+       <input type="text"  value="<?php echo 'Skander'?>" class="form-control" id="floatingInput">
   
     </div>
     <div class="form-floating mb-3">
-      <input type="text" class="form-control" id="floatingPassword" >
+      <input type="text"   value="<?php echo '15'?>" class="form-control" id="floatingPassword" >
   
     </div>
     <div class="form-floating mb-3">
-       <input type="text" class="form-control" id="floatingInput">
+       <input type="text"  value="<?php echo '9'?>" class="form-control" id="floatingInput">
   
     </div>
     <div class="form-floating mb-3">
-      <input type="text" class="form-control" id="floatingPassword" >
+      <input type="text"  value="<?php echo '8'?>" class="form-control" id="floatingPassword" >
   
     </div>
     <div class="form-floating mb-3">
-      <input type="text" class="form-control" id="floatingPassword" >
+      <input type="text"  value="<?php echo '16'?>" class="form-control" id="floatingPassword" >
   
     </div>
     <div class="form-floating mb-3">
-      <input type="text" class="form-control" id="floatingPassword" >
+      <input type="text" class="form-control"value="<?php echo'Votre moyenne est'.$moys ?>" id="floatingPassword" >
   
     </div>
 </div>
