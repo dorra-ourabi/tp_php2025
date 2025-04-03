e872d6fa282fc58da36f64b132122a02d479e546
dorra dorra <dorra.ourabi@insat.ucar.tn>
2025-04-03 12:20:26 +0100

    added css

--- ex_pokemon/attackpokemon.php
+++ ex_pokemon/attackpokemon.php
@@ -1,3 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        table {
+            border: 2px solid black; 
+            border-collapse: collapse; 
+            margin:20px 20px 20px 20px;
+            border-radius:2px;
+        }
+      .title{
+        background-color:rgb(152, 214, 233);
+        margin:20px 20px 20px 20px;
+      }
+      tr{
+        background-color:rgb(234, 228, 228);
+      }
+      
+    </style>
+</head>
+<body>
+    <div class="row">
+<div class="col container">
+<div class="form-floating mb-3">
+  <input type="text" class="form-control title" id="floatingInput" value="<?php echo'les combattants'?>" >
+</div>
+<table class="table" >
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+     
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      
+    </tr>
+  </tbody>
+</table>
+</div>
+<div class="col container"></div>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php
     class AttackPokemon{
         private int $attackMinimal;
