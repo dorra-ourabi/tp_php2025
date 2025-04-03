a70e5c2598651629dcb374274c7f61745eb80994
oumeima115 <oumeimahelal@gmail.com>
2025-04-03 10:41:23 +0100

    creation d'une page utilisateur

---
+++ ex_session/user.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   
+</head>
+<?php
+echo ' hello user!'.'<br>';
+session_start();
+
+
+ if (!isset($_SESSION['nb_visite'])){
+    $_SESSION['nb_visite']=1;
+echo 'Binevenue c votre premiére visite';}
+    else{
+    $_SESSION['nb_visite']=  $_SESSION['nb_visite']+1;
+echo 'merci pour voutre fidélité c votre '.$_SESSION['nb_visite'].'éme <br>';}?>
+<body>
+    <form  method="post">
+         <input type="submit" name="reset" value="rénitialiser">
+    </form>
+</body>
+</html>
